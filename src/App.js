9140331ebd3412365579e7727ecd1dbaaff5a365
DelfinaGalarza <dgalarzagraf@udesa.edu.ar>
2022-08-29 16:40:44 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './screens/Home/Home';
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+     <Header/>
+      <Switch>
+          <Route path ='/' exact={true} component={Home}/>
+        
+
+        </Switch>
+        <Footer/>
+      </div>    
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <p></p>
+    </header>
+  )
+}
+
+export default Header
