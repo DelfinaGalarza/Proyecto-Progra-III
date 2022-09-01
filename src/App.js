b858486d7f80ce15727fd7caeace336e98a65c66
DelfinaGalarza <dgalarzagraf@udesa.edu.ar>
2022-09-01 15:55:48 -0300

    favoritos

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import Home from './screens/Home/Home';
 
 
 
-
-
-
 function App() {
   return (
     <div>
---
+++ src/screens/Favoritos/Favoritos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Favoritos() {
+  return (
+    <footer>
+        <p></p>
+    </footer>
+  )
+}
+
+export default Favoritos
