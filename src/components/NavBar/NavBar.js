d5c920c693624edb169b860f20b1061d54d8fab2
candetallavi <ctallavi@udesa.edu.ar>
2022-09-08 13:26:34 -0300

    header

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,15 +5,34 @@ import './styles.css'
 
 function NavBar(props) {
   return (
-    
-    <header >
-      <div className='nav'>
-      <div > <Link to="/" exact="true"> HOME </Link></div>
-      <div >  <Link to="/favoritos" exact="true"> FAVORITOS</Link></div>
-      <div > <Link to="/vertodas"> VER TODAS</Link> </div>
-      </div>
-      
+  
+   
+
+
+    <header class="responsiveheader">
+       <div >
+    <title>SOUND-NOW</title>
+    <img src="./img/arriba.png" alt="" />
+    </div>
+        <section class="cajainicio">
+            <article class="header-items">
+                <article class="BarraDeBusqueda">
+                   <div className='menu'>
+                        <div > <Link to="/" exact="true"> HOME </Link></div>
+                        <div >  <Link to="/favoritos" exact="true"> FAVORITOS</Link></div>
+                        <div > <Link to="/vertodas"> VER TODAS</Link> </div>
+                        </div>
+                            {/* <form action="search-results.html" method="GET" class="buscar">
+                               <input type="text" name="search" placeholder="¿Qué estás buscando?" value="">
+                               <button type="submit" class="button">Buscar</button>
+                               <p class= "aviso"></p>
+                            </form> */}
+                        </article>
+                </article>
+         
+        </section>
     </header>
+
   )
 }
 
