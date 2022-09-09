7eb205e8bae08cd4a078bc89e3f1bbe4df9fa45b
haniaachaval <hachavalrodriguez@udesa.edu.ar>
2022-09-09 18:11:20 -0300

    sigo sin solucionar fav

--- src/screens/Favoritos/Favoritos.js
+++ src/screens/Favoritos/Favoritos.js
@@ -7,42 +7,61 @@ class Favoritos extends Component{
   constructor(){
       super()
       this.state={
-          favoritos:[], 
+          cancionesYalbumes:[], 
       }
   }
 
   componentDidMount(){
+      let fav =[];
       let favStorage = localStorage.getItem('favoritos')
 
       if(favStorage !== null){
-          let parsedArr = JSON.parse(favStorage) //parseamos el storage para obtener el array
-//hago fetch dentro de bucle 
-          Promise.all( //posibilita recibir un array completo de promesas que se cumplen o fallan con el map retornamos una promesa que es fectch
-            parsedArr.map(elm => {
-              return(fetch(`https://thingproxy.freeboard.io/fetch/https://api.deezer.com/album/${elm}`) //elm es cada id del array
-              .then(resp => resp.json())
-              .then( data =>{
-                console.log(data);
-                this.setState({
-                favoritos: this.state.favoritos.concat(data)
-            })})
-            .catch(e => console.log(e)
-            ))
-      }))
-    }
+          fav = JSON.parse(favStorage) //parseamos el storage para obtener el array
+          let favOk = [];
+
+          //recorro array y pedirla en el endpiont por los datos de cada fav
+          fav.forEach(unIdFavorito => { 
+            let url = `https://thingproxy.freeboard.io/fetch/https://api.deezer.com/album/${unIdFavorito}`
+            let url2= `https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks/${unIdFavorito}`
+            fetch(url + url2)
+            .then(resp => resp.json())
+              .then( data => favOk.push(data))
+              .then(() => this.setState({
+                cancionesYalbumes: favOk
+              }))
+            .catch(e => console.log(e))
+          })
+        }
   }
-    //pasarlo a un estado y esto a un map de cards que lo renderice. 
+//hago fetch dentro de bucle 
+  //         Promise.all( //posibilita recibir un array completo de promesas que se cumplen o fallan con el map retornamos una promesa que es fectch
+  //           parsedArr.map(elm => {
+  //             return(fetch(`https://thingproxy.freeboard.io/fetch/https://api.deezer.com/album/${elm}`) //elm es cada id del array
+  //             .then(resp => resp.json())
+  //             .then( data =>{
+  //               console.log(data);
+  //               this.setState({
+  //               favoritos: this.state.favoritos.concat(data)
+  //           })})
+  //           .catch(e => console.log(e)
+  //           ))
+  //     }))
+  //   }
+  // }
+  //   //pasarlo a un estado y esto a un map de cards que lo renderice. 
 
   render(){
+    console.log(this.state.cancionesYalbumes)
   return(
-    <section className="card-container" >
+          <React.Fragment>
+          
+                    <h2>Mis Albumes favoritos</h2>
+                    <section>
+                      {this.state.cancionesYalbumes.map((unfav, idx) => <CardCancion key={unfav + idx} datosfav={unfav}/>) }
+                    </section>
                     
-                    { 
-                    this.state.favoritos ?
-                        ( (fav, idx) => <CardCancion key={fav +idx} favoritos ={fav} />)
-                    : 'Cargando'
-                    }
-                </section>
+          </React.Fragment>
+    
   )
 }
 }
