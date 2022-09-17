9fe9cc19c293b16d1cfe962e861b2263ff75bd46
DelfinaGalarza <dgalarzagraf@udesa.edu.ar>
2022-09-17 18:48:11 -0300

    css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,16 +8,21 @@ function Navbar(){
 
     <header class="responsiveheader">
         <section class="cajainicio">
-        <h1 className='tituloprin'> RHYTHM</h1>
-            <article className="header-items">
+            <h1 className='tituloprin'>¡ RHYTHM !</h1> 
+
                   
+            <article className="header-items">
 
                         <div className="home"> <Link to="/" exact="true"> HOME </Link></div>
                         <div className="home">  <Link to="/favoritos" exact="true"> FAVORITOS</Link></div>
                         <div className="home"> <Link to="/vertodas/albumes"> ALBUMES</Link> </div>
                         <div className="home"> <Link to="/vertodas/podcast">PODCASTS</Link> </div>
                 
-                        <div className="home"> <img className="logo" src="./img/ritmo-parcial.jpg" alt="logo" /></div>
+                        <div className="home"> 
+                        
+                        <Link to= "/"> <img className="logo" src="./img/ritmo-parcial.jpg" alt="logo" /></Link> 
+                        
+                        </div>
                         
                      
                         
