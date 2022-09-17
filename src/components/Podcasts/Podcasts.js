149760033ace781b38a77622c9ea0e3e9e22ddcc
DelfinaGalarza <dgalarzagraf@udesa.edu.ar>
2022-09-17 11:09:12 -0300

    search de podcast

--- src/components/Podcasts/Podcasts.js
+++ src/components/Podcasts/Podcasts.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import './styles.css'
 import CardPodcast from '../CardPodcast/CardPodcast';
-// import Filtro from "../Filtro/Filtro";
+import Filtro from "../Filtro/Filtro";
+
 
 class Podcasts extends Component{
     constructor(){
@@ -27,44 +28,39 @@ class Podcasts extends Component{
             )
     }
 
-    // filtrarTarjetas(nombreBuscado){
-    //     let cancionesFiltradas= this.state.backup.filter(cancion => cancion.title.toLowerCase().includes(nombreBuscado.toLowerCase()));
-    //     this.setState({
-    //     canciones: cancionesFiltradas
-    //     })
 
-    //}
+    borrar(name){
+        let podcastsFiltrados = this.state.podcasts.filter(unpodcast => unpodcast.name !== name);
+        this.setState({
+            podcasts: podcastsFiltrados
+        })
+        }
+
+
+    filtrarPodcasts(nombre){
+        let podcastFiltrado= 
+        this.state.backup.filter
+        (podcast => podcast.title.toLowerCase().includes(nombre.toLowerCase()));
     
-    // traerMas(){
-    //     //Traer la siguiente página de personajes
-    //     fetch(this.state.nextUrl)
-    //         .then( res => res.json())
-    //         .then( data => this.setState({
-    //             canciones: data.results.concat(this.state.canciones),
-    //             nextUrl: data.info.next
-    //         }))
-    //         .catch()
-    // }
+        this.setState({
+        podcasts: podcastFiltrado
+        })
 
-    // borrar(id){
-    // let cancionesFiltradas = this.state.canciones.filter(unaCancion => unaCancion.id !== id);
-    // this.setState({
-    //     canciones: cancionesFiltradas
-    // })
-    // }
-
+    }
+    
 
     render(){
         return(
             <>
-             {/* <Filtro filtro ={(nombreBuscado)=> this.filtrarTarjetas(nombreBuscado)} />  */}
-            
-                {/* { <button onClick={()=>this.traerMas()}> Traer más </button> } */}
-                <section className="card-container" >
+             <Filtro filtro ={(nombre)=> this.filtrarPodcasts(nombre)} />        
+             <section className="card-container" >
                     
                     { 
                     this.state.podcasts.length >0 ?
-                        this.state.podcasts.map( (unPodcast, idx) => <CardPodcast key={unPodcast +idx} datosPodcast ={unPodcast} />)
+                        this.state.podcasts.map( (unPodcast, idx) => 
+                        <CardPodcast 
+                        key={unPodcast +idx} 
+                        datosPodcast ={unPodcast} />)
                     : 'Cargando'
                     }
                 </section>
