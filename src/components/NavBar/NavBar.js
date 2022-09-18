3553aa303359d27b148099526eae27d1ab2c34f7
haniaachaval <hachavalrodriguez@udesa.edu.ar>
2022-09-18 10:07:58 -0300

    mobile

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -24,7 +24,7 @@ function Navbar(){
                         
                         </div>
                         
-                     
+                    
                         
                 </article>
         
